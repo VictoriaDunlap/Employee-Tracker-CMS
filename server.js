525851ae63bae91b904e9ad22ac99ce670e3a880
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-08-07 08:46:05 -0400

    allEMployee and updateEmployeeRole created

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = mysql.createConnection(
   },
 );
 
-const initialQuestion = () => {
+const initialQuestion = async () => {
   inquirer.prompt([
   {
       type: 'list',
@@ -35,8 +35,9 @@ const initialQuestion = () => {
       case 'Add Employee':
         addEmployee()
       break;
-      // case 'Update Employee Role':
-      // break;
+      case 'Update Employee Role':
+        updateEmloyeeRole()
+      break;
       // case 'View All Roles':
       // break;
       // case 'Add Role':
@@ -52,16 +53,72 @@ const initialQuestion = () => {
 
 initialQuestion()
 
+// SELECT employee.manager_id FROM employee WHERE employee.manager_id <> employee.id
 // need to work on te join function 
-const allEmployees = () => {
-  db.query('SELECT * FROM employee', function (err, results) {
-  console.table(results);
+// const allEmployees = () => {
+//   db.query('SELECT employee.id, employee.first_name, employee.last_name, role_types.title, role_types.salary, department.dep_name, employee.manager_id FROM department INNER JOIN role_types ON role_types.department_id = department.id INNER JOIN employee ON role_types.id = employee.role_id', function (err, results) {
+//   console.table(results);
+//   })
+// }
+
+const addEmployee = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is their first name?',
+      name: 'firstName',
+    },
+    {
+    type: 'input',
+    message: 'What is their last name?',
+    name: 'lastName',
+    },
+    {
+    type: 'list',
+    message: 'What is their role?',
+    choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer'],
+    name: 'employeeRole',
+    },
+    {
+      type: 'input',
+      message: 'Who is their manager?',
+      name: 'employeeManager',
+    },
+  ])
+  
+  db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES = ?', function (err, results) {
+    console.table(results)
   })
 }
 
-const addEmployee = () => {
-  
-  db.query('')
+const updateEmloyeeRole = () => {
+  // collects employees from the database
+  const getEmployees = () => {
+  return new Promise((res, rej) => {
+    db.query(`SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS Name FROM employee`, (err,res) => {
+      console.table(res)
+    })
+  })
+}
+  const employeesToUpdate = getEmployees()
+  console.log(employeesToUpdate)
+  return new Promise((res,rej) => {
+    inquirer.prompt({
+      type: 'list',
+      message: 'Select employee.',
+      choices: [employeesToUpdate],
+      name: 'employeesToUpdateQuestion',
+    })
+  })
+}
+
+// collects employees from the database
+const getEmployees = () => {
+  return new Promise((res, rej) => {
+    db.query(`SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS Name FROM employee`, (err,res) => {
+      console.table(res)
+    })
+  })
 }
 
 app.use((req, res) => {
